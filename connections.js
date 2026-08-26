0793657dc565d8e723e4288fac53b9459023f62a
abouda20062018-alt <abouda20062018@gmail.com>
2026-08-26 11:09:42 +0100

    Add files via upload

---
+++ connections.js
@@ -0,0 +1,6 @@
+Parse.initialize(
+    "TON_APPLICATION_ID",
+    "TA_JAVASCRIPT_KEY"
+);
+
+Parse.serverURL = "https://backend.back4app.com/apps/6a142be2-f3b4-4450-94ea-56fbbea43198/browser";
---
+++ verif.js
@@ -0,0 +1,7 @@
+ch=document.getElementById(mdp).value;
+if(ch!="Eliteadmin"){
+    alert("mot de passe invalide");
+    
+}
+document.getElementById(ch1).innerHTML(welcome)
+
